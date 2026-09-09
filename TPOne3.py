6ac86baa55dbafc91709689d74ffb34258e95bb6
Sean D' Almeida <seanalmeidaofficial@gmail.com>
2026-09-09 16:57:44 +0530

    feat: update binary search on main branch

--- TPOne3.py
+++ TPOne3.py
@@ -40,4 +40,4 @@ obj = Selection_Sort_Binary_Search()
 obj.Selection_Sort(string_list)
 
 search_input = input("Enter the String : ")
-obj.Binary_Search(search_input, string_list)
+obj.Binary_Search(search_input, string_list)print("Main branch edit")
